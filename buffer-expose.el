69bfeb1feb9db7bb3f81dd3a21f74d9b3da71943
Clemens Radermacher <clemera@posteo.net>
2019-02-23 12:37:08 +0100

    Add binding for no stars

--- buffer-expose.el
+++ buffer-expose.el
@@ -52,9 +52,10 @@
 (defvar buffer-expose-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "<s-tab>") 'buffer-expose)
-    (define-key map (kbd "<C-tab>") 'buffer-expose-stars)
+    (define-key map (kbd "<C-tab>") 'buffer-expose-no-stars)
     (define-key map (kbd "C-c <C-tab>") 'buffer-expose-current-mode)
     (define-key map (kbd "C-c C-d") 'buffer-expose-dired-buffers)
+    (define-key map (kbd "C-c C-*") 'buffer-expose-stars)
     map)
   "Mode map for command `buffer-expose-mode'.")
 
